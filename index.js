294402d8acf950cd2e472ec7dd01825cdaa2225a
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-10-13 03:11:40 +0100

    finished index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const hourEl = document.getElementById("hours")
+const minuteEl = document.getElementById("minutes")
+const secondEl = document.getElementById("seconds")
+const ampmEl = document.getElementById("ampm")
+
+function updateClock(){
+    let hour = new Date().getHours()
+    let minutes = new Date().getMinutes()
+    let seconds = new Date().getSeconds()
+    let ampm = "AM"
+
+    if (hour > 12){
+        hour = hour - 12
+        ampm = "PM"
+    }
+
+    hourEl.innerText = hour;
+    minuteEl.innerText = minutes;
+    secondEl.innerText = seconds;
+    ampmEl, (innerText = ampm);
+    setTimeout(() => {
+        updateClock()
+    }, 1000)
+}
+
+updateClock()
